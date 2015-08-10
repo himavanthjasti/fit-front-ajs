c457cd20094f4142c458ccbea25506075e30a37c
Anushil Nandan <anushil.nandan@practo.com>
2015-08-10 15:40:23 +0000

    Initial COmmit

---
+++ app.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+
+    angular
+        .module('app', ['ngRoute', 'ngCookies','textAngular'])
+        .config(config)
+        .run(run);
+
+
+
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'home/home.view.html',
+                controllerAs: 'vm'
+            })
+            .when('/content', {
+                controller: 'ContentController',
+                templateUrl: 'content/home.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                controller: 'LoginController',
+                templateUrl: 'login/login.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'register/register.view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' });
+    }
+
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+    function run($rootScope, $location, $cookieStore, $http) {
+
+    }
+
+})();
---
+++ content/content.controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ContentController', ContentController);
+
+    function ContentController($scope, $http) {
+
+ 
+
+
+
+        // Our form data for creating a new post with ng-model
+
+        $scope.createPost = function() {
+
+            $http.get('http://fit.practo.local/content/');
+
+            var data = { 'title' : $scope.postTitle,'practo_account_id':1,'content':$scope.postTitle,'publishStatus':'PUBLISHED','tagid':'1'};
+
+            $http({
+                method: 'POST',
+                url: "http://fit.practo.local/content/",
+                headers: {'X-Profile-Token': '7eeebc0c-6079-4764-a294-43d4c7cbb743','Access-Control-Allow-Origin': '*'},
+                param: data
+            }).success(function () {});
+
+            /*$http.post("http://fit.practo.local/content/", {TEST: "TEST"}).success(function(responseData) {
+                //do stuff with response
+            });*/
+        }
+    }
+
+})();
