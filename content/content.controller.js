ba774097bf539f4e6b06dc1b1c000f9d85bdfac1
Anushil Nandan <anushil.nandan@practo.com>
2015-08-10 18:12:39 +0000

    routing

--- content/content.controller.js
+++ content/content.controller.js
@@ -15,15 +15,15 @@
 
         $scope.createPost = function() {
 
-            $http.get('http://fit.practo.local/content/');
+            $http.get('http://www.fit1.com/content/');
 
-            var data = { 'title' : $scope.postTitle,'practo_account_id':1,'content':$scope.postTitle,'publishStatus':'PUBLISHED','tagid':'1'};
+            var datax = { 'title' : $scope.postTitle,'practo_account_id':3,'content':$scope.postTitle,'publishStatus':'PUBLISHED','tagid':'1'};
 
             $http({
                 method: 'POST',
-                url: "http://fit.practo.local/content/",
-                headers: {'X-Profile-Token': '7eeebc0c-6079-4764-a294-43d4c7cbb743','Access-Control-Allow-Origin': '*'},
-                param: data
+                url: "http://www.fit1.com/content/",
+                data: $.param(datax), 
+                headers: {'X-Profile-Token': '487f909b-00a1-4680-85b8-33b3a9dc72cb', 'Content-Type': 'application/x-www-form-urlencoded'},
             }).success(function () {});
 
             /*$http.post("http://fit.practo.local/content/", {TEST: "TEST"}).success(function(responseData) {
