0bdd9bf112c4e36c1c43cbb00ba7fa7b5061d735
rdvs <rahul.gupat963@live.com>
2015-08-12 07:12:22 +0000

    updated commit

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-﻿(function () {
+(function () {
     'use strict';
 
 
@@ -13,46 +13,28 @@
     config.$inject = ['$routeProvider', '$locationProvider'];
     function config($routeProvider, $locationProvider) {
         $routeProvider
-            .when('/', {
-                controller: 'HomeController',
-                templateUrl: 'home/home.view.html',
-                controllerAs: 'vm'
-            })
             .when('/content', {
                 controller: 'ContentController',
-                templateUrl: 'content/home.view.html',
+                templateUrl: 'views/content/home.view.html',
                 controllerAs: 'vm'
             })
             .when('/content/:postId', {
                 controller: 'ContentController',
-                templateUrl: 'content/updatePost.view.html',
+                templateUrl: 'views/content/updatePost.view.html',
                 controllerAs: 'vm'
             })
             .when('/allcontent', {
                 controller: 'ContentController',
-                templateUrl: 'content/allcontent.view.html',
+                templateUrl: 'views/content/allcontent.view.html',
                 controllerAs: 'vm'
             })
             .when('/post/:postId', {
                 controller: 'ContentController',
-                templateUrl: 'content/post.view.html',
+                templateUrl: 'views/content/post.view.html',
                 controllerAs: 'vm'
             })
 
-
-            .when('/login', {
-                controller: 'LoginController',
-                templateUrl: 'login/login.view.html',
-                controllerAs: 'vm'
-            })
-
-            .when('/register', {
-                controller: 'RegisterController',
-                templateUrl: 'register/register.view.html',
-                controllerAs: 'vm'
-            })
-
-            .otherwise({ redirectTo: '/login' });
+            .otherwise({ redirectTo: '/allcontent' });
     }
 
     run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
