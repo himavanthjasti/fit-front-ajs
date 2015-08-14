fb6f03ebe375240c3787ecc926c3f521cd656717
himavanthjasti <himavanth.jasti@practo.com>
2015-08-14 10:24:27 +0000

    Image upload functionality

--- app/app.js
+++ app/app.js
@@ -3,15 +3,12 @@
 
 
     angular
-        .module('app', ['ngRoute', 'ngCookies','textAngular','siyfion.sfTypeahead','jsTag','tagService'])
+        .module('app', ['ngRoute', 'ngCookies','textAngular','siyfion.sfTypeahead','jsTag','tagService','ui.bootstrap', 'ngSanitize'])
         .config(config)
         .run(run);
 
-
-
-
     config.$inject = ['$routeProvider', '$locationProvider'];
-    function config($routeProvider, $locationProvider) {
+    function config($routeProvider, $locationProvider, $provide) {
         $routeProvider
             .when('/content', {
                 controller: 'ContentController',
@@ -33,7 +30,6 @@
                 templateUrl: 'views/content/post.view.html',
                 controllerAs: 'vm'
             })
-
             .otherwise({ redirectTo: '/allcontent' });
     }
 
@@ -41,5 +37,65 @@
     function run($rootScope, $location, $cookieStore, $http) {
 
     }
+
+    var app = angular.module('plunker', ['ui.bootstrap', 'textAngular', 'ngSanitize']);
+
+    app.config(['$provide',
+        function($provide) {
+            $provide.decorator('taOptions', ['taRegisterTool', '$modal', '$delegate',
+                function(taRegisterTool, $modal, taOptions) {
+                    // $delegate is the taOptions we are decorating
+                    // here we override the default toolbars specified in taOptions.
+                    taOptions.toolbar = [
+                        ['clear', 'h1', 'h2', 'h3'],
+                        ['ul', 'ol'],
+                        ['bold', 'italics'],
+                        ['insertLink', 'insertVideo']
+                    ];
+
+                    // Create our own insertImage button
+                    taRegisterTool('customInsertImage', {
+                        iconclass: "fa fa-picture-o",
+                        action: function() {
+                            var textAngular = this;
+                            var savedSelection = rangy.saveSelection();
+                            var modalInstance = $modal.open({
+                                // Put a link to your template here or whatever
+                                template: '<label>Enter the url to your image:</label><input type="text" ng-model="img.url"><button ng-click="submit()">OK</button>',
+                                size: 'sm',
+                                controller: ['$modalInstance', '$scope',
+                                    function($modalInstance, $scope) {
+                                        $scope.img = {
+                                            url: ''
+                                        };
+                                        $scope.submit = function() {
+                                            $modalInstance.close($scope.img.url);
+                                        };
+                                    }
+                                ]
+                            });
+
+                            modalInstance.result.then(function(imgUrl) {
+                                rangy.restoreSelection(savedSelection);
+                                textAngular.$editor().wrapSelection('insertImage', imgUrl);
+                            });
+                            return false;
+                        },
+                    });
+
+                    // Now add the button to the default toolbar definition
+                    // Note: It'll be the last button
+                    taOptions.toolbar[3].push('customInsertImage');
+                    return taOptions;
+                }
+            ]);
+        }
+    ]);
+
+    app.controller("MyCtrl", ['$scope',
+        function($scope) {
+            $scope.html = "Here's some sample text!!!.!!!";
+        }
+    ]);
 
 })();
