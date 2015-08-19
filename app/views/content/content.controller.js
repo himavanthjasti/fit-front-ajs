c53da5f2cf0a22d9a8b3f39dde5dfb0fdadd7857
root <root@practo-vagrant.dev>
2015-08-19 06:13:20 +0000

    css changes for rating functionality

--- app/views/content/content.controller.js
+++ app/views/content/content.controller.js
@@ -103,10 +103,8 @@ apps.controller('UploadImageModalInstance', function($scope, $modalInstance, Upl
         $scope.rating = 5;
 
         $scope.rateFunction = function(rating) {
-            console.log('Rating selected: ' + rating);
+            alert('Rating selected - ' + rating);
         };
-
-
 
         // Our form data for creating a new post with ng-model
         $scope.createPost = function() {
@@ -138,45 +136,42 @@ apps.controller('UploadImageModalInstance', function($scope, $modalInstance, Upl
 
     function starRating() {
         return {
-             restrict: 'EA',
-            template:
-                '<ul class="star-rating" ng-class="{readonly: readonly}">' +
-                '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="toggle($index)">' +
-                '    <i class="fa fa-star"></i>' + // or &#9733
-                '  </li>' +
-                '</ul>',
-            scope: {
-            ratingValue: '=ngModel',
-            max: '=?', // optional (default is 5)
-            onRatingSelect: '&?',
-            readonly: '=?'
-        },
-        link: function(scope, element, attributes) {
-            if (scope.max == undefined) {
-                scope.max = 5;
+             restrict : 'A',
+            template : '<ul class="rating">'
+                     + '    <li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
+                     + '\u2605'
+                     + '</li>'
+                     + '</ul>',
+            scope : {
+                ratingValue : '=',
+                max : '=',
+                onRatingSelected : '&'
+            },
+            link : function(scope, elem, attrs) {
+                var updateStars = function() {
+                    scope.stars = [];
+                    for ( var i = 0; i < scope.max; i++) {
+                        scope.stars.push({
+                            filled : i < scope.ratingValue
+                        });
+                    }
+                };
+                
+                scope.toggle = function(index) {
+                    scope.ratingValue = index + 1;
+                    scope.onRatingSelected({
+                        rating : index + 1
+                    });
+                };
+                
+                scope.$watch('ratingValue',
+                    function(oldVal, newVal) {
+                        if (newVal) {
+                            updateStars();
+                        }
+                    }
+                );
             }
-            function updateStars() {
-                scope.stars = [];
-                for (var i = 0; i < scope.max; i++) {
-                    scope.stars.push({
-                    filled: i < scope.ratingValue
-                });
-            }
-        };
-        scope.toggle = function(index) {
-          if (scope.readonly == undefined || scope.readonly === false){
-            scope.ratingValue = index + 1;
-            scope.onRatingSelect({
-              rating: index + 1
-            });
-          }
-        };
-        scope.$watch('ratingValue', function(oldValue, newValue) {
-          if (newValue) {
-            updateStars();
-          }
-        });
-      }
     };
   }
 
